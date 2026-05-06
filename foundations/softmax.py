4b4d16c88bf8bcff3e22ffcd3787ecd6e638680f
Mohamed Yassine Kallel <mohamedyassine.kallel@insat.ucar.tn>
2026-05-06 21:07:28 +0100

    Implement Softmax function (Math Foundations 3/6)

---
+++ foundations/softmax.py
@@ -0,0 +1,11 @@
+import numpy as np
+from numpy.typing import NDArray
+
+
+class Solution:
+
+    def softmax(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
+        z_s = z - np.max(z)
+        sum = np.sum(np.exp(z_s))
+        arr = np.exp(z_s) / sum
+        return np.round(arr, 4)
