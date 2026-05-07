9ac5054c727f1f9e436da6b3d630648e4e53aa93
Mohamed Yassine Kallel <mohamedyassine.kallel@insat.ucar.tn>
2026-05-08 00:54:12 +0100

    Implement RMS normalization (PyTorch 4/4)

---
+++ model/rms_normalization.py
@@ -0,0 +1,12 @@
+import numpy as np
+from typing import List
+
+
+class Solution:
+    def rms_norm(self, x: List[float], gamma: List[float], eps: float) -> List[float]:
+        x = np.array(x)
+        mean = np.mean(x, axis=0)
+        var = np.var(x, axis=0)
+        rms = np.sqrt(np.mean(x**2) + eps)
+        x_hat = x/rms
+        return np.round(gamma*x_hat, 4)
